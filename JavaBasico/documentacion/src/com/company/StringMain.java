e82d3aaeac63f73df32231193f6b3c2d519fddad
rjimenezg73 <robertojimenezg@gmail.com>
2023-05-12 00:45:30 -0600

    Leccion 1.24 - Métodos de la clase String

---
+++ JavaBasico/documentacion/src/com/company/StringMain.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class StringMain {
+  public static void main(String[] args) {
+    // Métodos de la clase String
+    /*
+        length()
+        startsWith()
+        endsWith()
+        indexOf()
+        subString(1,5)
+        trim()
+        equals()
+        compareTo()
+     */
+
+    String mensaje = "Hola Mundo";
+    System.out.println("La longitud de \'" + mensaje + "\' es de: " + mensaje.length());
+
+    String mensajeMays = mensaje.toUpperCase();
+    System.out.println(mensajeMays);
+
+    if(mensajeMays.equals(mensaje)){
+      System.out.println("Los textos son iguales");
+    }else{
+      System.out.println("Los textos no son iguales");
+    }
+
+    
+
+
+  }
+}

75fd10dc149e41f75c54c6e5f3c4030b19e23e3f
rjimenezg73 <robertojimenezg@gmail.com>
2023-05-13 14:28:23 -0600

    Seccion 5.5 a 5.6 - Clases, objetos, herencia y polimorfismo

--- JavaBasico/clases-objetos/src/poo/clases/Main.java
+++ JavaBasico/clases-objetos/src/poo/clases/Main.java
@@ -3,6 +3,19 @@ package poo.clases;
 public class Main {
 
   public static void main(String[] args) {
+    // Para crear un objeto
+    // Clase identificador = new MetodoConstructor();
+
+    // Crear un objeto utilizando el constructor vacío
+    Vehiculo toyotaPrius = new Vehiculo();
+
+    // Crear un objeto utilizando el constructor con parámetros
+    Vehiculo fordMondeo = new Vehiculo("Ford", "Mondeo", 2.1, 2010, false);
+    System.out.println(fordMondeo.facricante);
+    System.out.println(fordMondeo.modelo);
+    System.out.println(fordMondeo.speed);
+    fordMondeo.acelerar(60);
+    System.out.println("Velocidad despues de acelerar: " + fordMondeo.speed);
 
 
   }

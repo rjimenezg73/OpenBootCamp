2f3dbe692a41b3cb049074220ba5d5f58e3fa015
rjimenezg73 <robertojimenezg@gmail.com>
2023-06-05 23:14:02 -0600

    Comentarios importantes al inicio del proyecto Spring con Jersey

--- JavaAvanzado/demo/src/main/java/com/openbootcamp/demo/config/JerseyConfig.java
+++ JavaAvanzado/demo/src/main/java/com/openbootcamp/demo/config/JerseyConfig.java
@@ -4,10 +4,14 @@ import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
-@ApplicationPath("/")
-@Component
-public class JerseyConfig extends ResourceConfig {
+@ApplicationPath("/")   // Indica que nuestra aplicación se ejecuta despues del :8080
+@Component              // Se define ésta clase como un componente
+public class JerseyConfig extends ResourceConfig { // Debemos indicar que esta clase hereda de ResourceConfig
+   // Ahora creamos dentro de la configuración de Jersey un constructor
    public JerseyConfig(){
-      this.packages("com.openbootcamp.demo.controllers");
+
+      this.packages("com.openbootcamp.demo.controllers"); // Indicamos donde estarán nuestros controladores
+      // Un controlador es una clase de java que podemos llamar como deseamos
+      // para éste caso llamaremos BootcampersController
    }
 }
--- JavaAvanzado/demo/src/main/java/com/openbootcamp/demo/controllers/BootcampersController.java
+++ JavaAvanzado/demo/src/main/java/com/openbootcamp/demo/controllers/BootcampersController.java
@@ -4,12 +4,16 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import org.springframework.stereotype.Component;
 
+// Debemos configurar éste controlador diciendo a Spring que ésta clase es un componente
+// También debemos indicar qué es lo que va a procesar---- Barrita (path)
+// Los controladores deben contener métodos
 @Component
 @Path("/")
 public class BootcampersController {
 
-   @GET
+   @GET  // Implementando el método GET
    public String hola(){
+
       return "Hola";
    }
 
